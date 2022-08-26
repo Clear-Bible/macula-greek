f654ad45253092586456e4a060f81e20221287f8
Mike Brinker <themikejr@gmail.com>
2022-08-26 09:09:51 -0400

    Lowfat tests for parseable lowfat.

--- test/test_lowfat.py
+++ test/test_lowfat.py
@@ -1,8 +1,9 @@
 import pytest
 import os
 import codecs
+import re
 from lxml import etree
-from test import __lowfat_files__
+from test import __lowfat_files__, run_xpath_for_file
 
 
 @pytest.mark.parametrize("lowfat_file", __lowfat_files__)
@@ -17,8 +18,25 @@ def test_file_is_valid_utf8(lowfat_file):
     assert lines != ""
 
 
-# `etree.parse` throws errors is the xml is not valid.
 # This includes checking the format and uniqueness of @xml:id
+# `etree.parse` throws errors is the xml is not valid.
 @pytest.mark.parametrize("lowfat_file", __lowfat_files__)
 def test_file_is_valid_xml(lowfat_file):
     assert etree.parse(lowfat_file)
+
+
+@pytest.mark.parametrize("lowfat_file", __lowfat_files__)
+def test_ref_attr_correct_format(lowfat_file):
+    pattern = "^[A-Z0-9]{3} [0-9]+:[0-9]+![0-9]+$"  # USFM Ref
+    nodes = run_xpath_for_file("//w", lowfat_file)
+    for node in nodes:
+        assert node.attrib["ref"] != ""
+        assert re.match(pattern, node.attrib["ref"])
+
+
+def test_number_of_words():
+    total_count = 0
+    for lowfat_file in __lowfat_files__:
+        count = run_xpath_for_file("//w", lowfat_file)
+        total_count += len(count)
+    assert total_count == 137779
