d9e05e5d0b963966267c655aafb939964c548328
Jake Wegner <jake.wegner@biblica.com>
2023-10-06 11:32:37 -0500

    Remove old transform script

--- transform-to-lowfat.sh
+++
@@ -1,2 +0,0 @@
-ls -t Nestle1904/nodes/*.xml | xargs -n 1 basename | while read filename; do basex -W -i Nestle1904/nodes/$filename mappings/lowfat-macula-greek.xquery > Nestle1904/lowfat/$filename; done
-ls -t SBLGNT/nodes/*.xml | xargs -n 1 basename | while read filename; do basex -W -i SBLGNT/nodes/$filename mappings/lowfat-macula-greek.xquery > SBLGNT/lowfat/$filename; done
